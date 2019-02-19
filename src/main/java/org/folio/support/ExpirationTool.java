b8a24e8c5eee9028ab7bc7b624ef812220a97a3b
Kostyantyn_Khodarev <kostyantyn_khodarev@epam.com>
2019-02-19 11:46:33 +0200

    CIRCSTORE-109 & CIRCSTORE-110 Implement request statuses "Closed - Unfilled" and "Closed - Pickup expired"

--- src/main/java/org/folio/support/ExpirationTool.java
+++ src/main/java/org/folio/support/ExpirationTool.java
@@ -29,12 +29,13 @@ import static org.folio.rest.jaxrs.model.Request.Status.CLOSED_PICKUP_EXPIRED;
 
 public class ExpirationTool {
 
+  private static final Logger logger = LoggerFactory.getLogger(ExpirationTool.class);
+
   private ExpirationTool() {
     //do nothing
   }
 
   public static void doRequestExpiration(Vertx vertx, Context context) {
-    final Logger logger = LoggerFactory.getLogger(ExpirationTool.class);
     logger.info("Calling doRequestExpiration()");
     context.runOnContext(v -> {
       //Get a list of tenants
@@ -68,7 +69,6 @@ public class ExpirationTool {
   }
 
   public static Future<Integer> doRequestExpirationForTenant(Vertx vertx, Context context, String tenant) {
-    final Logger logger = LoggerFactory.getLogger(ExpirationTool.class);
     Future<Integer> future = Future.future();
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
     simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
@@ -76,7 +76,7 @@ public class ExpirationTool {
     context.runOnContext(v -> {
       PostgresClient pgClient = PostgresClient.getInstance(vertx, tenant);
       pgClient.setIdField("id");
-      String query = String.format("(status == \"%s\" AND requestExpirationDate < %s) OR (status == \"%s\" AND holdShelfExpirationDate < %s)",
+      String query = String.format("(status == \"%s\" AND requestExpirationDate < \"%s\") OR (status == \"%s\" AND holdShelfExpirationDate < \"%s\")",
         OPEN_NOT_YET_FILLED.value(), nowDateString, OPEN_AWAITING_PICKUP.value(), nowDateString);
       CQL2PgJSON cql2pgJson;
       CQLWrapper cqlWrapper;
@@ -89,29 +89,20 @@ public class ExpirationTool {
         return;
       }
       pgClient.get(REQUEST_TABLE, Request.class, fieldList, cqlWrapper, true, false, reply -> {
-        if(reply.failed()) {
+        if (reply.failed()) {
           logger.info(String.format("Error executing postgres query: '%s', %s",
             query, reply.cause().getLocalizedMessage()));
           future.fail(reply.cause());
-        } else if(reply.result().getResults().isEmpty()) {
+        } else if (reply.result().getResults().isEmpty()) {
           logger.info(String.format("No results found for query %s", query));
         } else {
           List<Request> requestList = reply.result().getResults();
-          List<Future> futureList = new ArrayList<>();
-          for(Request request : requestList) {
-            if (request.getStatus() == OPEN_NOT_YET_FILLED) {
-              request.setStatus(CLOSED_UNFILLED);
-            } else {
-              request.setStatus(CLOSED_PICKUP_EXPIRED);
-            }
-            Future<Void> saveFuture = saveRequest(vertx, context, tenant, request);
-            futureList.add(saveFuture);
-          }
+          List<Future> futureList = closeRequests(vertx, context, tenant, requestList);
           CompositeFuture compositeFuture = CompositeFuture.join(futureList);
           compositeFuture.setHandler(compRes -> {
             int succeededCount = 0;
-            for(Future fut : futureList) {
-              if(fut.succeeded()) {
+            for (Future fut : futureList) {
+              if (fut.succeeded()) {
                 succeededCount++;
               }
             }
@@ -123,9 +114,26 @@ public class ExpirationTool {
     return future;
   }
 
-  public static Future<Void> saveRequest(Vertx vertx, Context context, String tenant, Request request) {
-    final Logger logger = LoggerFactory.getLogger(ExpirationTool.class);
-    logger.info(String.format("Updating request with id %s", request.getId()));
+  private static List<Future> closeRequests(Vertx vertx, Context context, String tenant, List<Request> requestList) {
+    List<Future> futureList = new ArrayList<>();
+    if (!requestList.isEmpty()) {
+      for (Request request : requestList) {
+        if (request.getStatus() == OPEN_NOT_YET_FILLED) {
+          request.setStatus(CLOSED_UNFILLED);
+        } else {
+          request.setStatus(CLOSED_PICKUP_EXPIRED);
+        }
+        request.setPosition(null);
+        Future<Void> saveFuture = saveRequest(vertx, context, tenant, request);
+        futureList.add(saveFuture);
+      }
+    }
+    return futureList;
+  }
+
+  private static Future<Void> saveRequest(Vertx vertx, Context context, String tenant, Request request) {
+    String requestId = request.getId();
+    logger.info(String.format("Updating request with id %s", requestId));
     Future<Void> future = Future.future();
     context.runOnContext(v -> {
       try {
@@ -134,12 +142,12 @@ public class ExpirationTool {
         Criteria idCrit = new Criteria("ramls/request.json");
         idCrit.addField("'id'");
         idCrit.setOperation("=");
-        idCrit.setValue(request.getId());
+        idCrit.setValue(requestId);
         Criterion criterion = new Criterion(idCrit);
 
         pgClient.update(REQUEST_TABLE, request, criterion, true, updateReply -> {
           if(updateReply.failed()) {
-            logger.info(String.format("Error updating request %s: %s", request.getId(),
+            logger.info(String.format("Error updating request %s: %s", requestId,
               updateReply.cause().getLocalizedMessage()));
             future.fail(updateReply.cause());
           } else {
